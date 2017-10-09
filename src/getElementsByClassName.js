7bb20f1ecc3573749b0f769ae73e6b96452687f9
Hack Reactor Student <student@hackreactor.com>
2017-10-09 16:13:23 -0700

    Implement getElementByClassName using inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+  
+  var searchNodes = function(node) {
+    if (node) {
+      if (node.classList && node.classList.contains(className)) {
+        results.push(node);
+      }
+      if (node.hasChildNodes()) {
+        node.childNodes.forEach(searchNodes);
+      }
+    }
+  };
+  
+  searchNodes(document.body);
+  return results;
+
 };
